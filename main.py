1dc09a758332400983b69e85936e926f1a42b50c
courtrightj7 <36347248+courtrightj7@users.noreply.github.com>
2018-02-17 11:27:36 -0500

    test change

--- main.py
+++ main.py
@@ -4,7 +4,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, JC!'
+  connection = pymysql.connect(host='35.224.87.205',
+                             user='courtrightj7',
+                             password='Omerta77!',
+                             db='BitLynx',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+  cursor = connection.cursor()
+  sql = "SELECT `id`, `password` FROM `testtable` WHERE `email`=%s"
+  cursor.execute(sql, ('webmaster@python.org',))
+  result = cursor.fetchone()
+  connection.close()
+  return result
 
 if __name__ == '__main__':
   app.run()
+
+
+
