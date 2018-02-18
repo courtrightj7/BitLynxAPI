f24fcd5bea895b5acf2c096b63bc7fd2c5e264f8
courtrightj7 <36347248+courtrightj7@users.noreply.github.com>
2018-02-17 22:28:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,12 @@
-from flask import Flask
-import pymysql
+from flask import Flask, request
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-  connection = pymysql.connect(host='35.224.87.205',
-                             user='courtrightj7',
-                             password='Omerta77!',
-                             connect_timeout=1000,
-                             db='BitLynx',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
-  cursor = connection.cursor()
-  sql = "SELECT `id`, `password` FROM `testtable` WHERE `email`=%s"
-  cursor.execute(sql, ('webmaster@python.org',))
-  result = cursor.fetchone()
-  connection.close()
-  return result
+def hello_world():)
+  return "Hello World"
 
 if __name__ == '__main__':
   app.run()
