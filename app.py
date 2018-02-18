07564238b897348250429b63764474f18684aecc
courtrightj7 <36347248+courtrightj7@users.noreply.github.com>
2018-02-17 22:35:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask, request
-from flask_restful import Resource, Api
-from sqlalchemy import create_engine
-from json import dumps
+from flask import Flask
+from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():)
-  return "Hello World"
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+    <img src="http://loremflickr.com/600/400" />
+    """.format(time=the_time)
 
 if __name__ == '__main__':
-  app.run()
-
-
+    app.run(debug=True, use_reloader=True)
