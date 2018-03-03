93c177d8d1744ccbfaa48fb78bb271d29ac0221e
courtrightj7 <36347248+courtrightj7@users.noreply.github.com>
2018-03-03 18:03:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ from json import dumps
 from datetime import datetime
 import psycopg2
 
-DATABASE_URL = os.environ['DATABASE_URL']
-
+#DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = 'TEST'
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
  
 app = Flask(__name__)
 api = Api(app)
 
-@app.route('/', methods = ['POST'])
+@app.route('/test', methods = ['POST'])
 def Test():
     if request.method == 'POST':
         message = str(DATABASE_URL)
