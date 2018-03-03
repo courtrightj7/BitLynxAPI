a505eee639f352f00ef46f9701472f608d76e21a
courtrightj7 <36347248+courtrightj7@users.noreply.github.com>
2018-03-03 15:50:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,61 +1,29 @@
+import os
 from flask import Flask, request
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
 from datetime import datetime
+import psycopg2
 
-e = create_engine('sqlite:///BitLynx.db')
+DATABASE_URL = os.environ['DATABASE_URL']
+
+try:
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+except:
+    pass
+ 
 app = Flask(__name__)
 api = Api(app)
 
-@app.route('/Login', methods = ['POST'])
-def Login():
- 
-    if request.method == 'POST':
-        data = request.form # a multidict containing POST data
-        username = str(data['username'])
-        password = str(data['password'])
-        
-        conn = e.connect()
-        # Perform query and return JSON data
-        SQL = "select CustomerID from LoginTable where UserName = '" + username + "' and Password = '" + password +"'"
-        
-        query = conn.execute(SQL)
-        x = query.cursor.fetchall()  
-        conn.close()
- 
-        if len(x) > 0:
-            x = x[0][0]
-            x = str(x)
-            return str({'CustomerID': x})
+@app.route('/', methods = ['GET'])
+def Test():
+    if request.method == 'GET':
+        if conn:
+            message = 'Successful'
         else:
-            return str({'Message':'User Name or Password Incorrect'})
-#########################################################################################################################################
-@app.route('/Registration', methods = ['POST'])
-def RegisterAccount():
- 
-    if request.method == 'POST':
-        data = request.form # a multidict containing POST data
-        username = str(data['username'])
-        password = str(data['password'])
-        company = str(data['company'])
-        conn = e.connect()
-        mySQL = "Select * from LoginTable where "
-        mySQL += "UserName = '"+str(username) + "'"
-        query = conn.execute(mySQL)
-        x = query.cursor.fetchall()
-        conn.close()
-        if len(x) > 0:
-            message = str({'Message':'User Already Exists'})
-        else:
-            conn = e.connect()
-            mySQL = "Insert into LoginTable (UserName,Password,Company) values('"+ str(username) 
-            mySQL += "','" + str(password) + "','"+str(company)+"')"
-            query = conn.execute(mySQL)
-            #conn.commit()
-            conn.close()
-            message =  str({'Message':mySQL})
-        return message
+            message = 'Unsuccessful'
+
         
 if __name__ == '__main__':
      app.run()
